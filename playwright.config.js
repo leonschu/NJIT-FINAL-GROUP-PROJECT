4fe36b9aff0470a55c98bb5cc7361af299d451e7
leonschu <leon@lschulte.de>
2023-11-11 14:07:06 -0500

    cd back

--- playwright.config.js
+++ playwright.config.js
@@ -60,7 +60,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "cd mv-app && npm run dev",
+    command: "cd mv-app && npm run dev && cd ..",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
