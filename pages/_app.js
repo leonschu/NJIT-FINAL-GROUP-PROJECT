2512b67d5e3fb0cb9633359906f8ef29cc70ec13
leonschu <leon@lschulte.de>
2023-12-02 16:19:05 -0500

    let's try this Google Ad integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,23 @@
 import "@styles/globals.scss";
+import { useEffect } from 'react';
+import Router from 'next/router';
+import { initGA, logPageView } from '../lib/ga';
 export default function App({ Component, pageProps }) {
+
+    useEffect(() => {
+        initGA();
+        
+        const handleRouteChange = () => {
+          logPageView();
+        };
+    
+        Router.events.on('routeChangeComplete', handleRouteChange);
+    
+        return () => {
+          Router.events.off('routeChangeComplete', handleRouteChange);
+        };
+      }, []);
+
     return <Component {...pageProps} />;
 }
+
