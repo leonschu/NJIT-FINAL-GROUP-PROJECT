71f365150eaf4684d03e07711ed4230364e2989c
leonschu <leon@lschulte.de>
2023-12-02 17:08:06 -0500

    Cookie consent added and google analytics only when agreed

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,59 @@
 import "@styles/globals.scss";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Router from 'next/router';
 import { initGA, logPageView } from '../lib/ga';
+import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
+
 export default function App({ Component, pageProps }) {
+    const [isConsentGiven, setIsConsentGiven] = useState(false);
 
     useEffect(() => {
-        initGA();
-        
+        // Check if consent has already been given
+        const consent = getCookieConsentValue();
+        if (consent === "true") {
+            initGA();
+            setIsConsentGiven(true);
+        }
+
+        // Handle route changes for Google Analytics
         const handleRouteChange = () => {
-          logPageView();
+            if (isConsentGiven) {
+                logPageView();
+            }
         };
-    
+
         Router.events.on('routeChangeComplete', handleRouteChange);
-    
+
         return () => {
-          Router.events.off('routeChangeComplete', handleRouteChange);
+            Router.events.off('routeChangeComplete', handleRouteChange);
         };
-      }, []);
+    }, [isConsentGiven]);
 
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <CookieConsent
+                location="bottom"
+                buttonText="I accept"
+                declineButtonText="I decline"
+                cookieName="myAwesomeCookieName"
+                style={{ background: "#2B373B" }}
+                buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+                declineButtonStyle={{ color: "#4e503b", fontSize: "13px" }}
+                onAccept={() => {
+                    initGA();
+                    setIsConsentGiven(true);
+                }}
+                onDecline={() => {
+                    setIsConsentGiven(false);
+                }}
+                enableDeclineButton
+                flipButtons
+                expires={150}
+            >
+                This website uses cookies to enhance the user experience.{" "}
+                <span style={{ fontSize: "10px" }}>Your choice will be saved for your next visit.</span>
+            </CookieConsent>
+        </>
+    );
 }
-
