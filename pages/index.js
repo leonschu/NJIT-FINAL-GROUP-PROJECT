8b426898de5d696650e69c17aff12c36b8b5b2fc
leonschu <leon@lschulte.de>
2023-12-03 16:01:48 -0500

    done

--- pages/index.js
+++ pages/index.js
@@ -29,106 +29,31 @@ export default function Home() {
                 <HomeBanner />
                 {/* Components Container */}
                 <SectionContainer className="components--container wrap wrap-px grid gap-8 sm:gap-24">
+                <div className="mt-5" style={{ background: 'linear-gradient(103deg, #382E0A -1.23%, #A69D74 99.83%)', height: '2px' }}></div>
                     {/* Features */}
                     <MotionBTTContainer
                         transition={{ delay: 0.2, duration: 0.5 }}
                     >
                         <SectionContainer id="features" className="features">
                             <BadgeGroup alignment="center">
-                                <BadgeMessage>Features</BadgeMessage>
+                                <BadgeMessage><b>Our Purpose</b></BadgeMessage>
                             </BadgeGroup>
-                            <PageTitle
-                                className="text-center mx-auto"
-                                type="default"
-                            >
-                                Simplify Your Nutrition Journey with NutriTrack
-                            </PageTitle>
                             <Content className="text-center" alignment="center">
                                 <p>
-                                    Hey there! Welcome to NutriTrack, the
-                                    ultimate nutrition meal planner powered by
-                                    Notion. We&apos;ve got some awesome features
-                                    lined up to make your nutrition journey a
-                                    piece of cake (pun intended). Check them
-                                    out:
+                                Elite Business Café is a specialty coffee shop designed specifically for affluent business professionals, such as salespeople, lawyers, and executives. It focuses on providing an exclusive atmosphere conducive to business meetings and deal closures, along with high-quality specialty coffee and a premium service experience.
                                 </p>
+
+                                <div className="mt-5" style={{ background: 'linear-gradient(103deg, #382E0A -1.23%, #A69D74 99.83%)', height: '2px' }}></div>
                             </Content>
-                            <div className="mt-5" style={{ background: 'linear-gradient(103deg, #382E0A -1.23%, #A69D74 99.83%)', height: '2px' }}></div>
                             <ContentImage />
+                        
                         </SectionContainer>
-                    </MotionBTTContainer>
-                    {/* Card Container Tabs */}
-                    <MotionBTTContainer
-                        transition={{ delay: 0.2, duration: 0.5 }}
-                    >
-                        <SectionContainer className="feature-tabs">
-                            <BadgeGroup alignment="center">
-                                <BadgeMessage>More Features</BadgeMessage>
-                                <BadgeIcon icon="twemoji:waving-hand" />
-                            </BadgeGroup>
-                            <PageTitle
-                                className="text-center mx-auto"
-                                type="default"
-                            >
-                                Master Your Meal Planning and Nutrition Journey
-                            </PageTitle>
-                            <Content className="text-center" alignment="center">
-                                <p>
-                                    Our comprehensive Notion template designed
-                                    to empower you on your meal planning and
-                                    nutrition journey. With our user-friendly
-                                    features, customizable layouts, and seamless
-                                    recipe integration, taking control of your
-                                    meals has never been easier.
-                                </p>
-                            </Content>
-                            <CardGroup className="grid scroll-m-24 gap-8 grid-cols-1 max-w-4xl mx-auto mt-24 md:grid-cols-2">
-                                <Card className="col-span-1 text-primary-900">
-                                    <CardBody className="w-full bg-white-600/20 p-12">
-                                        <CardImage
-                                            src="/features4.png"
-                                            alt="Customizable Layouts image used."
-                                        />
-                                        <CardHeader className="!text-black !text-2xl !font-bold">
-                                            Customizable Layouts
-                                        </CardHeader>
-                                        <p>
-                                            Personalize your meal planning
-                                            experience with our flexible
-                                            layouts. Tailor your sections,
-                                            categories, and tabs to suit your
-                                            unique style and organization
-                                            preferences. Our template adapts to
-                                            your needs, providing a seamless and
-                                            personalized planning experience.
-                                        </p>
-                                    </CardBody>
-                                </Card>
-                                <Card className="col-span-1 text-primary-900">
-                                    <CardBody className="w-full bg-white-600/20 p-12">
-                                        <CardImage
-                                            src="/features3.png"
-                                            alt="Progress Tracking image used."
-                                        />
-                                        <CardHeader className="!text-black !text-2xl !font-bold">
-                                            Progress Tracking
-                                        </CardHeader>
-                                        <p>
-                                            Celebrate your wins and stay
-                                            motivated on your nutrition journey.
-                                            NutriTrack allows you to monitor
-                                            your progress with weight,
-                                            measurements, and other key metrics.
-                                            Track your improvements over time
-                                            and see the positive impact of your
-                                            healthy choices.
-                                        </p>
-                                    </CardBody>
-                                </Card>
-                            </CardGroup>
+
+                        <SectionContainer id="features" className="features">
                         </SectionContainer>
-                        <div className="mt-5" style={{ background: 'linear-gradient(103deg, #382E0A -1.23%, #A69D74 99.83%)', height: '2px' }}></div>
+
                     </MotionBTTContainer>
+                    
                     {/* Testimonials */}
                     <MotionBTTContainer
                         transition={{ delay: 0.2, duration: 0.5 }}
@@ -139,7 +64,6 @@ export default function Home() {
                         >
                             <BadgeGroup alignment="left">
                                 <BadgeMessage>Testimonials</BadgeMessage>
-                                <BadgeIcon icon="twemoji:waving-hand" />
                             </BadgeGroup>
                             <PageTitle className="" type="default">
                                 This is what our customers have to say
