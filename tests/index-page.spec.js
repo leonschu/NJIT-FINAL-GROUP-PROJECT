2f42cc3f197ec842fdbb0a9de184dad0c9561d57
leonschu <leon@lschulte.de>
2023-12-16 22:56:13 -0500

    try 3

--- tests/index-page.spec.js
+++ tests/index-page.spec.js
@@ -7,6 +7,13 @@ test('simple equality check', async ({}) => {
 });
 
 test('Check Page Title', async ({ page }) => {
+    // Replace with your Next.js application's URL
+    await page.goto('http://localhost:3000');
+  
+    // Wait for the title to be set to the expected value
+    await page.waitForFunction(() => document.title === 'Elite Business Caffee - Home');
+  
+    // Retrieve the title and assert its value
     const title = await page.title();
     expect(title).toBe('Elite Business Caffee - Home');
   });
