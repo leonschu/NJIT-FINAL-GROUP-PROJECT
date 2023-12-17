eda28aefe02b7225eedb1cedf2fbec5c57bc7bef
leonschu <leon@lschulte.de>
2023-12-17 17:18:39 -0500

    faq outsourced

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -1,71 +1,8 @@
 import { SectionContainer } from "@components/Section";
 import { Icon } from "@iconify/react";
 import { useState } from "react";
-import { v4 as uuid } from "uuid";
 import clsx from "clsx";
-
-const accordionData = [
-    {
-        id: uuid(),
-        title: "Does this website work in zero gravity?",
-        isOpen: true,
-        content: "Absolutely! We're proud to say our website remains 100% functional in zero gravity environments. Astronauts, rejoice!"
-    },
-    {
-        id: uuid(),
-        title: "Can I access the website with my mind?",
-        isOpen: true,
-        content: "We're still working on telepathic interfaces. For now, please use a standard computer or mobile device."
-    },
-    {
-        id: uuid(),
-        title: "Is it true that this website was once mistaken for an undiscovered planet?",
-        isOpen: true,
-        content: "While we can't confirm nor deny this, we do like to think our website is out of this world!"
-    },
-    {
-        id: uuid(),
-        title: "Will using this site increase my IQ?",
-        isOpen: true,
-        content: "We can't promise an IQ boost, but you'll definitely feel smarter!"
-    },
-    {
-        id: uuid(),
-        title: "Can I find the meaning of life on your site?",
-        isOpen: true,
-        content: "Our site is full of wonders, but for life's big questions, we recommend a good book or a long walk."
-    },
-    {
-        id: uuid(),
-        title: "Is it safe to visit your website during a full moon?",
-        isOpen: true,
-        content: "Yes, and it’s also werewolf-approved for night browsing."
-    },
-    {
-        id: uuid(),
-        title: "Do you offer a discount to time travelers?",
-        isOpen: true,
-        content: "Yes, but only if you visited us yesterday."
-    },
-    {
-        id: uuid(),
-        title: "Can my pet access your website?",
-        isOpen: true,
-        content: "While designed for humans, tech-savvy pets might find it intriguing!"
-    },
-    {
-        id: uuid(),
-        title: "Is it true that this website can bake cookies?",
-        isOpen: true,
-        content: "Only the digital kind, unfortunately. We're still perfecting our recipe for chocolate chip."
-    },
-    {
-        id: uuid(),
-        title: "If I get lost on your website, do you offer GPS navigation?",
-        isOpen: true,
-        content: "Not yet, but we do provide a handy search bar to help you find your way."
-    }
-];
+import accordionData from '../../content/faq_data';
 
 const accordionItemType = {
     top: "rounded-t-lg",
