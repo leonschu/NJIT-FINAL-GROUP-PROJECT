d8e265840b4cec5678f42b08b86fb97b0b466b61
leonschu <leon@lschulte.de>
2023-12-16 23:00:20 -0500

    tests passed

--- tests/index-page.spec.js
+++ tests/index-page.spec.js
@@ -1,14 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-const localhost = "http://localhost:3000/index";
-
-test('simple equality check', async ({}) => {
-  test.expect(1).toBe(1);
-});
+const index_page = "http://localhost:3000/index";
 
 test('Check Page Title', async ({ page }) => {
     // Replace with your Next.js application's URL
-    await page.goto('http://localhost:3000');
+    await page.goto(index_page);
   
     // Wait for the title to be set to the expected value
     await page.waitForFunction(() => document.title === 'Elite Business Caffee - Home');
