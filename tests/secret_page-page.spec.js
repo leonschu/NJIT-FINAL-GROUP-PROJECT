0012b5d6d26b7f28f44d6f1a2f8540d28cea42ca
leonschu <leon@lschulte.de>
2023-12-16 23:13:09 -0500

    all tests included

--- tests/secret_page-page.spec.js
+++ tests/secret_page-page.spec.js
@@ -1,15 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-const index_page = "http://localhost:3000/index";
+const index_page = "http://localhost:3000/secret_page";
 
 test('Check Page Title', async ({ page }) => {
     // Replace with your Next.js application's URL
     await page.goto(index_page);
   
     // Wait for the title to be set to the expected value
-    await page.waitForFunction(() => document.title === 'Elite Business Caffee - Home');
+    await page.waitForFunction(() => document.title === 'Elite Business Caffee - Secret Page');
   
     // Retrieve the title and assert its value
     const title = await page.title();
-    expect(title).toBe('Elite Business Caffee - Home');
+    expect(title).toBe('Elite Business Caffee - Secret Page');
   });
