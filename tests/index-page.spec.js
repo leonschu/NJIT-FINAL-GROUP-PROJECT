f3fa42a1b38c6f51839e486123ee43d662d66a30
leonschu <leon@lschulte.de>
2023-12-16 22:46:16 -0500

    test added

---
+++ tests/index-page.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+const localhost = "http://localhost:3000";
+
+test('simple equality check', async ({}) => {
+  test.expect(1).toBe(1);
+});
+
+test('Check Page Title', async ({ page }) => {
+    const title = await page.title();
+    expect(title).toBe('Elite Business Caffee - Home');
+  });
