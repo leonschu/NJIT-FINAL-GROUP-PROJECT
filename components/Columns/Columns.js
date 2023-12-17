1c2ec2906ef671c0200b5d99c25c73899566eebe
leonschu <leon@lschulte.de>
2023-12-17 17:29:36 -0500

    changed the next few

--- components/Columns/Columns.js
+++ components/Columns/Columns.js
@@ -1,44 +1,12 @@
 import { SectionContainer } from "@components/Section";
 import { Icon } from "@iconify/react";
-import { v4 as uuid } from "uuid";
-
-const ColumnData = [
-    {
-        id: uuid(),
-        title: "Jamie L. - Aspiring Astronaut",
-        icon: "carbon:user-avatar-filled-alt",
-        content:
-        "I used to think rocket science was hard, then I found this website. Now, I'm pretty sure I could build a spaceship in my backyard. Bonus points for not having to wear a space suit while browsing!"
-    },
-    {
-        id: uuid(),
-        title: "Alex D. - Professional Cat Herder",
-        icon: "carbon:user-avatar-filled-alt",
-        content:
-        "Managing a herd of cats is a walk in the park compared to finding a good website. But this site? It's like a giant catnip for my organizational needs. My feline friends and I give it two paws up!"
-    },
-    {
-        id: uuid(),
-        title: "Morgan R. - Time Travel Enthusiast",
-        icon: "carbon:user-avatar-filled-alt",
-        content:
-        "I've visited websites from 1999 to 2050, and this is by far the best one yet! It's so good, I decided not to go back to the future. Now, if only I could remember where I parked my time machine..."
-    },
-    {
-        id: uuid(),
-        title: "Casey B. - Underwater Basket Weaver",
-        icon: "carbon:user-avatar-filled-alt",
-        content:
-        "Who knew underwater basket weaving had so much in common with web design? This site is a breath of fresh air – figuratively speaking, of course. I can’t weave baskets underwater and surf the web at the same time, but if I could, I would!"
-    }
-];
-
+import testimonials_data from '../../content/testimonials_data';
 
 
 export const Columns = () => {
     return (
         <SectionContainer className="benefits-lists grid gap-x-8 gap-y-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-16">
-            {ColumnData.map((item) => (
+            {testimonials_data.map((item) => (
                 <div
                     id={item.id}
                     key={item.id}
--- components/ContentImage/ContentImage.js
+++ components/ContentImage/ContentImage.js
@@ -3,29 +3,16 @@ import { Icon } from "@iconify/react";
 import Image from "next/image";
 import { v4 as uuid } from "uuid";
 
-const ContentImageData = [
-    {
-        id: uuid(),
-        title: "We care about your money",
-        content:
-            "At Elite Business Café, we understand that money isn't just important – it's everything. Our exclusive environment caters to those who know the value of a dollar and the thrill of multiplying it. Here, we don't just serve coffee; we serve opportunities to expand your wealth. Remember, they say money can't buy happiness, but we've never seen anyone frown in a limo, clad in a $10,000 suit and a Rolex. Let us be your partner in your journey to unparalleled financial success.",
-        align: "right",
-        image: "/persona.png"
-    },
-    {
-        id: uuid(),
-        title: "A Refuge for Power Lunches and Networking",
-        content:
-            "In the hushed elegance of our cafe, our coveted couches provide the perfect setting for closing business deals, forging new connections, and exchanging ideas with influential individuals. The soft ambiance, the impeccable service, and the aroma of freshly brewed coffee create an atmosphere conducive to both creativity and productivity. Dont miss out on this opportunity and get your phrase to be able to reserve a table today.",
-        align: "left",
-        image: "/street.png"
-    }
-];
+import menu_data from '../../content/menu_data';
+import home_content_image from '../../content/home_content_image';
+
+
+
 
 export const ContentImage = () => {
     return (
         <SectionContainer className="process-items mt-16 space-y-16">
-            {ContentImageData.map((item) => (
+            {home_content_image.map((item) => (
                 <div
                     id={item.id}
                     key={item.id}
@@ -175,53 +162,12 @@ export const ContentImage_inside = () => {
 };
 
 
-const ContentImageData_menu = [
-    {
-        id: uuid(),
-        title: "Cosmic Caramel Nebula",
-        content:
-            "Immerse yourself in the wonders of the cosmos with our Cosmic Caramel Nebula. This exquisite concoction is a blend of rich espresso and frothed milk, elegantly swirled with caramel and sprinkled with edible gold dust, evoking the shimmering stars in a galaxy. A touch of cinnamon adds a spice of mystery, mirroring the endless exploration of space. This drink is a tribute to the innovative spirit and creative minds at NJIT, crafted for those who draw inspiration from the stars. It's not just a coffee; it's an experience that fuels the imagination of entrepreneurs and thinkers alike, making it a unique and exclusive offering on our menu.",
-        align: "left",
-        image: "/drink1.png"
-    },
-    {
-        id: uuid(),
-        title: "Quantum Quencher Mocha",
-        content:
-            "For the daring and the innovators, the Quantum Quencher Mocha is a testament to the bold and the brave. This drink merges the rich intensity of dark chocolate with the robust flavor of double espresso, balanced perfectly with steamed milk. But what truly sets it apart is the hint of chili, adding an unexpected twist that challenges the palate, much like the unpredictable nature of quantum physics. Topped with quantum foam whipped cream, it embodies the cutting-edge spirit of technology and innovation. It's crafted for those who aren't afraid to push boundaries, making it a unique addition to our menu that resonates with risk-takers and tech enthusiasts.",
-        align: "right",
-        image: "/drink2.png"
-    },
-    {
-        id: uuid(),
-        title: "Engineer's Espresso Elixir",
-        content:
-            "Precision and simplicity find their perfect blend in the Engineer's Espresso Elixir. This drink features a single-origin espresso, delicately enhanced with a drop of hazelnut syrup and complemented by the smoothness of steamed oat milk. The crowning touch is a tiny, edible gear-shaped chocolate, symbolizing the intricate and detailed work of engineers. This drink is a nod to the focused and meticulous nature of engineering, made with a rare coffee bean known for its clarity-inducing properties. It's a special brew, exclusively crafted for those who appreciate the fine details in both their work and their coffee, making it a distinctive and beloved choice on our menu.",
-        align: "left",
-        image: "/drink3.png"
-    },
-    {
-        id: uuid(),
-        title: "Architect’s Aromatic Alchemy",
-        content:
-            "The Architect’s Aromatic Alchemy is a masterpiece of flavor and design. This visually stunning drink combines light roast coffee with a delicate infusion of lavender in the milk foam, creating a soothing aroma. The foam is artistically drizzled with honey and dusted with cocoa powder, resembling the intricate details found in architectural designs. It's a tribute to the art of architecture, blending aesthetics with flavor in a way that appeals to those with a keen eye for design. The unique artisanal approach and calming effect of this drink make it an exclusive and sought-after item on our menu, particularly among those who appreciate beauty in both form and taste.",
-        align: "right",
-        image: "/drink4.png"
-    },
-    {
-        id: uuid(),
-        title: "Biotech Brew",
-        content:
-            "The Biotech Brew is a harmonious fusion of health and science. This innovative drink combines the refreshing qualities of green tea with the energizing kick of espresso, enhanced with a hint of mint extract and the zest of lemon. The swirl of spirulina, forming a DNA helix design atop the drink, not only adds a nutrient boost but also symbolizes the power and potential of biotechnology. This drink is specially crafted for health-conscious innovators and biotech enthusiasts, offering a unique blend that caters to both well-being and energy needs. It stands out on our menu as an exclusive choice for those who are passionate about the intersection of health, science, and coffee.",
-        align: "left",
-        image: "/drink5.png"
-    }
-];
+
 
 export const ContentImage_menu = () => {
     return (
         <SectionContainer className="process-items mt-16 space-y-16">
-            {ContentImageData_menu.map((item) => (
+            {menu_data.map((item) => (
                 <div
                     id={item.id}
                     key={item.id}
