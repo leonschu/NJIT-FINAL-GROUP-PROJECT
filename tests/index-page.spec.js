88e15bf828d86eec4e1b1add565ac09ad67edf66
Gabriel Trojanowski <gabetrojanowski@outlook.com>
2023-12-17 22:11:43 -0500

    Close #122

--- tests/index-page.spec.js
+++ tests/index-page.spec.js
@@ -2,6 +2,8 @@ import { test, expect } from "@playwright/test";
 
 const index_page = "http://localhost:3000";
 
+
+// Test #1 Check Page Title
 test('Check Page Title', async ({ page }) => {
     // Replace with your Next.js application's URL
     await page.goto(index_page);
@@ -13,3 +15,14 @@ test('Check Page Title', async ({ page }) => {
     const title = await page.title();
     expect(title).toBe('Elite Business Caffee - Home');
   });
+
+  // Test #2 Menu Navigation Test
+  test('Homepage should load with the correct title', async ({ page }) => {
+    // Navigate to the homepage
+    await page.goto('https://njit-final-group-project.vercel.app');
+ 
+ 
+    // Check if the title is correct
+    await expect(page).toHaveTitle('Elite Business Caffee - Home');
+ });
+ 
