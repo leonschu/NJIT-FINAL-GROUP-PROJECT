66ca181bae4d307a4ed4d44020251ef6754dc94f
Gabriel Trojanowski <gabetrojanowski@outlook.com>
2023-12-18 00:00:08 -0500

    Check Menu Navigation from FAQ

--- tests/faq-page.spec.js
+++ tests/faq-page.spec.js
@@ -1,4 +1,3 @@
-
 import { test, expect } from "@playwright/test";
 
 const index_page = "http://localhost:3000/faq";
@@ -14,3 +13,20 @@ test('Check Page Title', async ({ page }) => {
     const title = await page.title();
     expect(title).toBe('Elite Business Caffee - FAQ');
   });
+
+    // Test #2 Menu Navigation Test
+ test('Navigation to Menu page works correctly', async ({ page }) => {
+  // Navigate to the homepage
+  await page.goto('https://njit-final-group-project.vercel.app/faq');
+ 
+ 
+  // Click on the Menu link
+  await page.click("a[href='/menu']"); // Using the href attribute as the selector
+ 
+ 
+  // Check if the URL is correct
+  await expect(page).toHaveURL('https://njit-final-group-project.vercel.app/menu');
+ 
+ 
+  // Add any additional assertions if needed, like checking for a specific element on the Menu page
+ });
