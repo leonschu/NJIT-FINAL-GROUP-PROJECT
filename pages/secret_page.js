2e7ced5a51fa696d151eff9cc071f502aeec31b0
leonschu <77119042+leonschu@users.noreply.github.com>
2023-12-19 00:57:21 -0500

    Revert "Merge pull request #244 from leonschu/243-lighthouse-performance-is-very-low"
    This reverts commit eb841d5df2b92127bcf5fc8bb76a6cfdf58942ef, reversing
    changes made to f53a560c8e356b61f9d58525accf44a77a417c13.

--- pages/secret_page.js
+++ pages/secret_page.js
@@ -1,10 +1,21 @@
 import { BadgeMessage, BadgeGroup, BadgeIcon } from "@components/Badge";
 import { SectionContainer } from "@components/Section";
+import { PageTitle } from "@components/Title";
 import { Layout } from "@components/Layout";
-import { HomeBanner_short  } from "@components/Banner";
+import { HomeBanner, HomeBanner_short  } from "@components/Banner";
+import { Columns } from "@components/Columns";
 import { ContentImage_loyalty } from "@components/ContentImage";
+import { Content } from "@components/Content";
+import { Accordion } from "@components/Accordion";
 import { MotionBTTContainer } from "@components/Motion";
 import SEO from "@components/SEO/SEO";
+import {
+    CardBody,
+    CardGroup,
+    CardHeader,
+    CardImage,
+    Card
+} from "@components/Card";
 
 export default function Home() {
     return (
